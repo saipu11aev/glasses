73edcc0206794cc8405141a6d67637bf04a38fef
Gusennn <gusen14049@gmail.com>
2023-10-17 17:16:09 +0300

    add Tailwind 2

--- glasses-app/src/App.js
+++ glasses-app/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+        <h1 className="text-3xl font-bold underline gray">
+            Hello world!
+        </h1>
     </div>
   );
 }
